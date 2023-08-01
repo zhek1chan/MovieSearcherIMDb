1a9ed2e8cbbf72d4d250d8da0205052cb444dc46
zhek1chan <evg.ermolov@yandex.ru>
2023-08-01 18:38:56 +0500

    MVC ready

--- app/src/main/java/ru/kryu/dreamkinopoisk/domain/api/MoviesRepository.kt
+++ app/src/main/java/ru/kryu/dreamkinopoisk/domain/api/MoviesRepository.kt
@@ -1,7 +1,8 @@
 package ru.kryu.dreamkinopoisk.domain.api
 
 import ru.kryu.dreamkinopoisk.domain.models.Movie
+import ru.kryu.dreamkinopoisk.ui.util.Resource
 
 interface MoviesRepository {
-    fun searchMovies(expression: String): List<Movie>
+    fun searchMovies(expression: String): Resource<List<Movie>>
 }
--- app/src/main/java/ru/kryu/dreamkinopoisk/ui/poster/PosterActivity.kt
+++ app/src/main/java/ru/kryu/dreamkinopoisk/ui/poster/PosterActivity.kt
@@ -2,23 +2,17 @@ package ru.kryu.dreamkinopoisk.ui.poster
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.ImageView
-import com.bumptech.glide.Glide
+import ru.kryu.dreamkinopoisk.ui.util.Creator
 import ru.kryu.dreamkinopoisk.R
 
 class PosterActivity : Activity() {
 
-    private lateinit var poster: ImageView
+    private val posterController = Creator.providePosterController(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poster)
-        poster = findViewById(R.id.poster)
-        val url = intent.extras?.getString("poster", "")
-
-        Glide.with(applicationContext)
-            .load(url)
-            .into(poster)
+        posterController.onCreate()
     }
 }
 /*class PosterActivity : AppCompatActivity() {
